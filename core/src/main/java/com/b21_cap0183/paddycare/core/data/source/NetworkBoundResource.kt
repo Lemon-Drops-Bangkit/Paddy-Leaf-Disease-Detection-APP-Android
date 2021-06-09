e39d689e7047fbc60c8a6def0a80391f97d3808c
Muhammad Rifki <69866073+rifki-sgnic@users.noreply.github.com>
2021-06-10 00:11:28 +0700

    Minor Changes

--- core/src/main/java/com/b21_cap0183/paddycare/core/data/source/NetworkBoundResource.kt
+++ core/src/main/java/com/b21_cap0183/paddycare/core/data/source/NetworkBoundResource.kt
@@ -8,30 +8,22 @@ abstract class NetworkBoundResource<ResultType, RequestType> {
 
     private var result: Flow<Resource<ResultType>> = flow {
         val dbSource = loadFromDB().first()
-        Log.d("1", "tolol")
         if (shouldFetch(dbSource)) {
-            Log.d("1", shouldFetch(dbSource).toString())
             emit(Resource.Loading())
-            Log.d("Load", "ing")
             when (val apiResponse = createCall().first()) {
                 is ApiResponse.Success -> {
-                    Log.d("Load1", apiResponse.toString())
                     saveCallResult(apiResponse.data)
                     emitAll(loadFromDB().map { Resource.Success(it) })
-                    Log.d("Succ", "ess")
                 }
                 is ApiResponse.Empty -> {
-                    Log.d("Load2", apiResponse.toString())
                     emitAll(loadFromDB().map { Resource.Success(it) })
                 }
                 is ApiResponse.Error -> {
-                    Log.d("Load3", apiResponse.toString())
                     onFetchFailed()
                     emit(Resource.Error<ResultType>(apiResponse.errorMessage))
                 }
             }
         } else {
-            Log.d("1", shouldFetch(dbSource).toString())
             emitAll(loadFromDB().map { Resource.Success(it) })
         }
     }
